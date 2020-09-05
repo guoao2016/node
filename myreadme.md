69555a2dfeee679674341e9d88640b01726a620e
1044914567 <1044914567@qq.com>
2020-09-05 10:16:18 +0800

    update: node

--- myreadme.md
+++ myreadme.md
@@ -2,3 +2,22 @@
 
 ###
  cpu-stat
+
+
+### node持久化 mysql
+1. 文件系统数据库   -- json文件
+2. mysql
+[mysql安装](https://www.cnblogs.com/winton-nfs/p/11524007.html)
+
+1. 安装服务  mysqld --install
+2. 初始化mysql mysqld --initialize --console  s,yUXaz!?9Vy
+3. 开启mysql   net start mysql
+4. mysql -u root -p
+s,yUXaz!?9Vy
+
+
+
+
+mysqld --defaults-file="C:\mysql\mysql-8.0.20-winx64\my.ini" --console --skip-grant-tables
+
+[[docker][win10]安装的坑](https://www.jianshu.com/p/09d53c822cf8)
--- node-01/02-fs.js
+++ node-01/02-fs.js
@@ -1,16 +1,21 @@
-const fs = require('fs')
-// const data = fs.readFileSync('./package.json')
-// console.log(data)   // 二进制
-// console.log(data.toString('utf-8'))
+// const fs = require('fs')
+// // const data = fs.readFileSync('./package.json')
+// // console.log(data)   // 二进制
+// // console.log(data.toString('utf-8'))
 
-const {promisify} = require('util');
-const readFile = promisify(fs.readFile);
+// const {promisify} = require('util');
+// const readFile = promisify(fs.readFile);
 
-// fs.readFileSync('./package.json', (err, data) => {
-//     console.log(data.toString('utf-8'))
+// // fs.readFileSync('./package.json', (err, data) => {
+// //     console.log(data.toString('utf-8'))
+// // })
+
+// readFile('./package.json').then( data => {
+//     console.log(data.toString())
 // })
 
-readFile('./package.json').then( data => {
-    console.log(data.toString())
+const fs = require('fs')
+fs.readFile('./package.json',(err, data)=>{
+    
 })
 
